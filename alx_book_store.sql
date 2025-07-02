2b61185168c9275909218607190b2c48f51575d4
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-07-02 22:19:33 +0300

    Fixed

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,37 +1,37 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 use alx_book_store;
 CREATE TABLE Authors(
-author_id int primary key auto_increment,
-author_name varchar(215)
+author_id int PRIMARY KEY auto_increment,
+author_name VARCHAR(215)
 );
 CREATE TABLE Books(
-book_id int primary key auto_increment,
-title varchar(130),
+book_id int PRIMARY KEY auto_increment,
+title VARCHAR(130),
 author_id int,
-price double, 
-publication_date date,
-foreign key (author_id) references Authors(author_id)
+price DOUBLE, 
+publication_date DATE,
+FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Customers(
-customer_id int primary key auto_increment,
-customer_name varchar(215),
-email varchar(215),
-address text
+customer_id int PRIMARY KEY auto_increment,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
 );
 
 CREATE TABLE Orders(
-order_id int primary key auto_increment,
+order_id int PRIMARY KEY auto_increment,
 customer_id int,
-order_date date,
-foreign key (customer_id) references Customers(customer_id)
+order_date DATE,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
-orderdetailid int primary key auto_increment,
+orderdetailid int PRIMARY KEY auto_increment,
 order_id int,
 book_id int,
-quantity double,
-foreign key (order_id) references Orders(order_id),
-foreign key (book_id) references Books(book_id)
+quantity DOUBLE,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
